5fce4bab9877e284a4157ad58dd18ad757b0e7b7
sghill <steve@sghill.net>
2013-02-16 14:24:25 -0500

    function to tack on 'px'

--- main.js
+++ main.js
@@ -9,13 +9,17 @@ var score = document.getElementById('score');
 
 pile.addEventListener('touchmove', function(e) {
   _.each(e.touches, function(touch) {
-    pile.style.left = touch.pageX + "px";
-    pile.style.top = touch.pageY + "px";
+    pile.style.left = toPixels(touch.pageX);
+    pile.style.top = toPixels(touch.pageY);
   });
 }, false);
 
 pile.addEventListener('touchend', function() {
   score.innerHTML = parseInt(score.innerHTML) + 5;
-  pile.style.left = ((window.screen.width - pile.style.width) / 2) + "px";
-  pile.style.top = ((window.screen.height - pile.style.height) / 2) + "px";
+  pile.style.left = toPixels((window.screen.width - pile.style.width) / 2);
+  pile.style.top = toPixels((window.screen.height - pile.style.height) / 2);
 }, false);
+
+function toPixels(x) {
+  return [x,"px"].join("");
+}
