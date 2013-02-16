bb3543b62e8bab15128782ebf58745ddad0da22a
sghill <steve@sghill.net>
2013-02-16 15:21:04 -0500

    some primitive collision detection

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 document.body.addEventListener('touchmove', function(e) { e.preventDefault(); }, false);
 
 var pile = document.getElementById('pile');
-var greenTarget = document.getElementById('green');
-var blueTarget = document.getElementById('blue');
-var redTarget = document.getElementById('red');
-var yellowTarget = document.getElementById('yellow');
+var targets = {
+  green: document.getElementById('green'),
+  blue: document.getElementById('blue'),
+  yellow: document.getElementById('yellow'),
+  red: document.getElementById('red')
+};
+
 var score = document.getElementById('score');
 
 pile.addEventListener('touchmove', function(e) {
@@ -15,10 +18,29 @@ pile.addEventListener('touchmove', function(e) {
 }, false);
 
 pile.addEventListener('touchend', function() {
-  score.innerHTML = parseInt(score.innerHTML) + 5;
+  var pilePosition = {
+    left: parseInt(pile.style.left),
+    right: parseInt(pile.style.left) + 50,
+    bottom: parseInt(pile.style.top) + 50,
+    top: parseInt(pile.style.top)
+  };
+
+  if(pilePosition.left < 75 && pilePosition.top < 75) {
+    score.innerHTML = "hit green";
+  } else if (pilePosition.left < 75 && pilePosition.bottom > (window.innerHeight - 75)){
+    score.innerHTML = "hit blue";
+  } else if(pilePosition.right > (window.innerWidth - 75) && pilePosition.top < 75) { 
+    score.innerHTML = "hit red";
+  } else if(pilePosition.right > (window.innerWidth - 75) && pilePosition.bottom > (window.innerHeight - 75)) {
+    score.innerHTML = "hit yellow";
+  } else {
+    score.innerHTML = "missed!";
+  }
+
   pile.style.left = toPixels((window.screen.width - pile.style.width) / 2);
   pile.style.top = toPixels((window.screen.height - pile.style.height) / 2);
 }, false);
+
 
 function toPixels(x) {
   return [x,"px"].join("");
